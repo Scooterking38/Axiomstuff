6e84aadc363b201b6fae16e1944f62da893b7c3d
Scooterking38 <161932463+Scooterking38@users.noreply.github.com>
2026-02-06 17:53:22 +0000

    Fix factor calculation logic in Main.py
    Refactor factor calculation to use integer division and correct condition for factors.

--- Main.py
+++ Main.py
@@ -3,9 +3,9 @@ y = input('enter 2nd clause eg y-3: ')
 z = int(input('enter result: '))
 i,list1 = 1,[]
 #factors
-while i <= z/2:
-  d = z/i
-  if d == round(d) and (z == 2 or not d == 2):
+while i <= z//2:
+  d = z//i
+  if d % i == 0 and (z == 2 or not d == 2):
     i1,i2,i3,i4 = (i,d),(d,i),(0-i,0-d),(0-d,0-i)
     list1.extend([i1,i2,i3,i4])
   i += 1
