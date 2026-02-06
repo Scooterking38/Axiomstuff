bf1ccebf00a7c025cfd6751127ee6b0472c29ee9
Scooterking38 <161932463+Scooterking38@users.noreply.github.com>
2026-02-06 07:43:27 +0000

    Add items to list1 in Main.py
    Append calculated items to list1 for further processing.

--- Main.py
+++ Main.py
@@ -10,6 +10,10 @@ while i >= z/2:
     item2 = (div,i)
     item3 = (0-i,0-div)
     item4 = (0-div,0-i)
+    list1.append(item1)
+    list1.append(item2)
+    list1.append(item3)
+    list1.append(item4)
   i += 1
 x2 = x[1:]
 y2 = y[1:]
