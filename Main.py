ec4f54e48c3151e271e314d36347c3596ea2b58f
Scooterking38 <161932463+Scooterking38@users.noreply.github.com>
2026-02-06 07:17:40 +0000

    Refactor Main.py with calculations and list logic
    Refactor Main.py to include calculations and list generation.

--- Main.py
+++ Main.py
@@ -1,2 +1,19 @@
-thing = input('enter thing')
-#
+x = 'x+2'
+y = 'y-3'
+z = 3
+list1 = []
+i = 1
+while i >= z/2:
+  div = z//i
+  if div %% 1 == 0 and (z == 2 or not div == 2):
+    list1.append((i,div))
+    list1.append((div,i))
+    list1.append((0-i,0-div))
+    list1.append((0-div,0-i))
+  i += 1
+x2 = x
+y2 = y
+del x2[0]
+del y2[0]
+x2 = 0-int(x2)
+y2 = 0-int(y2)
