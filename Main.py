4df8e7c97e4093929e7b0e3b275d170ef37e197b
Scooterking38 <161932463+Scooterking38@users.noreply.github.com>
2026-02-07 08:44:39 +0000

    Improve input parsing and output display
    Refactor input handling and output formatting for clarity.

--- Main.py
+++ Main.py
@@ -1,23 +1,27 @@
 x = input('enter 1st clause eg x+2: ')
 y = input('enter 2nd clause eg y-3: ')
 z = int(input('enter result: '))
+
 i, list1 = 1, []
 
-if '+' in x: var1, num1 = x.split('+')
-elif '-' in x: var1, num1 = x.split('-')
+if '+' in x:
+    var1, num1 = x.split('+')
+elif '-' in x:
+    var1, num1 = x.split('-')
     num1 = '-' + num1
-if '+' in y: var2, num2 = y.split('+')
-elif '-' in y: var2, num2 = y.split('-')
+
+if '+' in y:
+    var2, num2 = y.split('+')
+elif '-' in y:
+    var2, num2 = y.split('-')
     num2 = '-' + num2
 
-# factors
 while i <= abs(z):
     if z % i == 0:
         d = z // i
-        list1.extend([(i, d),
-                      (-i, -d)])
+        list1.extend([(i, d), (-i, -d)])
     i += 1
 
-#output
 for a, b in list1:
-    print(var1 + ': ' + str(a + int(num1)), var2 + ': ' + str(b + int(num2)))
+    print(var1 + ': ' + str(a - int(num1)),
+          var2 + ': ' + str(b - int(num2)))
